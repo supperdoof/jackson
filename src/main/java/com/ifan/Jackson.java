98a7b84d2f7c017cd5522d578d80c7c4512247ae
季刚 <jigang.yf@alibaba-inc.com>
2018-12-28 16:00:30 +0800

    URL Decode

--- src/main/java/com/ifan/Jackson.java
+++ src/main/java/com/ifan/Jackson.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.net.URLDecoder;
 import java.util.List;
 
 public class Jackson {
@@ -85,6 +86,26 @@ public class Jackson {
             JSONObject object = JSONObject.parseObject(jsonArray.get(i).toString());
             System.out.println(object.getString("movie"));
         }
+
+
+        //URL Decode
+        String str2 = "e93d03558.em21,/data/logs/ssjs.log,1545836401.903`2018-12-26T23:00:01+08:00`11.224.21.60"
+            + "`ut2.shuqistat.com`-`POST /3.gif HTTP/1.1`200`185`-`shuqireader/4.0.7 (iPhone; iOS 12.1; Scale/2.00)"
+            + "`-`-`adts=1545836401&ai=app_ydsc&apv=4.0.7.1&argnum=3&brd=Apple&ext=&fr=&imei"
+            + "=4D0B42149CD7EDBA97D5A5092CA362DB78148980&ipli=111111&istsad=n&log=%7B%22app%22%3A%5B%7B%22arg2%22%3A"
+            + "%5B%7B%22cid%22%3A%22723727%22%2C%22time%22%3A%5B%7B%22stime%22%3A%221545836395%22%2C%22etime%22%3A"
+            + "%221545836396%22%7D%2C%7B%22stime%22%3A%221545836396%22%2C%22etime%22%3A%221545836401%22%7D%5D%7D%5D"
+            + "%2C%22arg3%22%3A%22n%22%2C%22arg1%22%3A%226720460%22%7D%5D%7D&mnf=Apple&mod=iPhone8&msdk=8.0.0&pbd"
+            + "=181205&plf=iOS&pli=111111&reqEncryptParam=%3A&reqEncryptType=-1&resEncryptType=-1&sdk=12.1&sn"
+            + "=A57E8D9F48EF80BCF5EE07784772915956B52517&ts=1545836401&user_id=1084801556&wh=750x1334`0.001";
+
+        try{
+            System.out.println(URLDecoder.decode(str2,"UTF-8"));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
 
 
 
